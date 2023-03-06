f6dd775b909a6f84245aad4cf14c2a0a3d1dfeba
XShirleyXST <shutong1@ualberta.ca>
2023-03-06 14:24:33 -0700

    Connect Firestore with project code

--- QRHunterLeaderboard/app/src/main/java/com/example/myapplication/MainActivity.java
+++ QRHunterLeaderboard/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,12 +8,14 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ListView playerRanks;
     ArrayAdapter<String> adapter;
-
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
