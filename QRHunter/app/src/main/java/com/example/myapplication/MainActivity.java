df23f3eccddbba0185c671efe45fc0658a463c7c
woopy098 <78347904+woopy098@users.noreply.github.com>
2023-03-08 18:50:26 -0700

    Revert "Added functionality of bottom navigation bar; Anika added code to ope…"

--- QRHunter/app/src/main/java/com/example/myapplication/MainActivity.java
+++ QRHunter/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,30 +1,13 @@
 package com.example.myapplication;
 
-/** Reference(s):
- * Using setOnItemSelectedListener to make bottom navigation bar functional:
- * -- From: www.stackoverflow.com
- * -- URL: https://stackoverflow.com/q/67641594
- * -- Author: https://stackoverflow.com/users/10429009/ali-moghadam
- *
- */
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
-import android.content.Intent;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarItemView;
-import com.google.android.material.navigation.NavigationBarMenu;
-import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.List;
@@ -38,31 +21,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.highest_scores);
-
-        BottomNavigationView bottomNavigationView  = (BottomNavigationView) findViewById(R.id.nav_bar);
 
         // Testing leaderboard screen (highest scores)
         String names[] = {
                 "Harry", "Draco", "Ron", "Hermione"
         };
+        setContentView(R.layout.highest_scores);
         getSupportActionBar().setTitle("Leaderboard");
         playerRanks = findViewById(R.id.player_ranks);
         adapter = new ArrayAdapter<String> (this, R.layout.userranks, R.id.username, names);
         playerRanks.setAdapter(adapter);
 
-        // Bottom Navigation bar functionality
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-                    if (item.getItemId() == R.id.camera) {
-                        try {
-                            Intent intent = new Intent();
-                            intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-                            startActivity(intent);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    return true;
-                });
+
     }
 }
