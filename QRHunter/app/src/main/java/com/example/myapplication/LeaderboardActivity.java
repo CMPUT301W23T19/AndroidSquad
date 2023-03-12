1c815a41bd7534571eef188cfbe32ec694452ead
XShirleyXST <shutong1@ualberta.ca>
2023-03-12 01:02:49 -0700

    Player's ranking
    display player's ranking for the highest scoring QRcodes

--- QRHunter/app/src/main/java/com/example/myapplication/LeaderboardActivity.java
+++ QRHunter/app/src/main/java/com/example/myapplication/LeaderboardActivity.java
@@ -5,13 +5,19 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class LeaderboardActivity extends AppCompatActivity {
     ListView playerRanks;
@@ -44,6 +50,34 @@ public class LeaderboardActivity extends AppCompatActivity {
             return true;
         });
 
+        db = FirebaseFirestore.getInstance();
+        CollectionReference playerRef = db.collection("Player");
+        Query query = playerRef.orderBy("highestScore", Query.Direction.DESCENDING);
+        query.get().addOnSuccessListener(queryDocumentSnapshots -> {
+            List<HashMap<String, Object>> players = new ArrayList<>();
+            for (DocumentSnapshot document : queryDocumentSnapshots) {
+                HashMap<String, Object> playerData = new HashMap<>();
+                playerData.put("Username", document.getString("Username"));
+                playerData.put("highestScore", document.getLong("highestScore"));
+                players.add(playerData);
+            }
+
+            // Find the rank of the player with the given name
+            String userName = "anna46";
+            int playerRank = -1;
+            for (int i = 0; i < players.size(); i++) {
+                String name = (String) players.get(i).get("Username");
+                if (name.equals(userName)) {
+                    playerRank = i + 1;
+                    break;
+                }
+            }
+
+            // Set the text of the playerRank TextView to display the player's rank
+            String playerRankText = "Your rank(highest scoring): " + playerRank;
+            TextView playerRankTextView = findViewById(R.id.player_rank);
+            playerRankTextView.setText(playerRankText);
+        });
 
     }
  
