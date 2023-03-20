d68fbd2bda565d48a163d1683bf7dd3b65a4ffc9
woopy098 <randy.luna98@ymail.com>
2023-03-20 16:46:19 -0600

    with QR code camera and permission made by randy and anika

--- QRHunter/app/src/main/java/com/example/myapplication/CameraController.java
+++ QRHunter/app/src/main/java/com/example/myapplication/CameraController.java
@@ -1,18 +1,26 @@
 package com.example.myapplication;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
 public class CameraController {
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+
     private final Context context;
     private final ActivityResultLauncher<ScanOptions> barLauncher;
     private QRCodeControllerDB qrControllerDB;
@@ -22,14 +30,38 @@ public class CameraController {
         this.context = context;
         this.barLauncher = barLauncher;
     }
+
     public void scanCode() {
+        // Check if location permission is granted before launching the barcode scanner
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            launchBarcodeScanner();
+        } else {
+            // Location permission is not granted, request for it
+            ActivityCompat.requestPermissions((Activity) context,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        }
+    }
+
+    // Called after requesting for location permission
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Location permission granted, launch barcode scanner
+                launchBarcodeScanner();
+            }
+        }
+    }
+
+    // Helper method to launch the barcode scanner with options
+    private void launchBarcodeScanner() {
         ScanOptions options = new ScanOptions();
         options.setPrompt("volume up to flash on!!");
         options.setBeepEnabled(true);
         options.setOrientationLocked(true);
         options.setCaptureActivity(CameraActivity.class);
         barLauncher.launch(options);
-
     }
 
     public ActivityResultLauncher<ScanOptions> getBarLauncher() {
@@ -45,6 +77,5 @@ public class CameraController {
     public void handleScanResult(String contents, FirebaseFirestore db, Context context) {
         qrCodeControllerDB = new QRCodeControllerDB(contents, "anna46", db);
         qrCodeControllerDB.validateAndAdd(context);
-
     }
 }
